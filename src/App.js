d13e3dcc34a7f760577eb49ac50b9ca8ec2a3f19
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-15 14:44:09 +0300

    new components implemented to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DownloadButton from "./components/DownloadButton/DownloadButton";
+import ContactButton from "./components/ContactButton/ContactButton";
+import Navbar from "./components/Navbar/Navbar";
+import WelcomeText from "./components/WelcomeText/WelcomeText";
+import Shapes from "./components/Shapes/Shapes";
+import AccountList from "./components/AccountList/AccountList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Shapes />
+      <Navbar>
+        <ContactButton />
+        <DownloadButton />
+      </Navbar>
+      <WelcomeText />
+      <AccountList/>
     </div>
   );
 }
