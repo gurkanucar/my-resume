0e8915f44c704c5960d161ee3a461ac723fd62b6
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-15 20:03:36 +0300

    static datas updated

--- src/assets/data/achievement.js
+++ src/assets/data/achievement.js
@@ -3,6 +3,6 @@ export const achievements = [
     title: "Yasam Kafesi (Cage of Life)",
     jobDescription:
       "With this project we designed after the Corlu Train Accident on 8 July 2018; In train journeys, people are not thrown out of the windows when the train overturns and possible loss of life is prevented. I developed the project with Arduino. I won the 3rd place in the competition organized by the Trakya Development Agency.",
-    date: "04/2021",
+    startDate: "04/2019",
   },
 ];
