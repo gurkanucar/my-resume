84a4ff6ca944b72fbb36e86d295a4dae312b2c5d
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-15 16:22:20 +0300

    cover letter component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import LandingPageComponent from "./components/LandingPageComponent/LandingPageComponent";
+import CoverLetterComponent from "./components/CoverLetterComponent/CoverLetterComponent";
 
 function App() {
   return (
     <div>
       <LandingPageComponent />
+      <CoverLetterComponent/>
     </div>
   );
 }
---
+++ src/components/CoverLetterComponent/CoverLetterComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./CoverLetterComponent.css";
+
+const CoverLetterComponent = () => {
+  return (
+    <div className="cover-letter-component__root">
+      <span className="cover-letter-component__name">Gurkan UCAR</span>
+      <span className="cover-letter-component__title">
+        ~Java Backend Developer
+      </span>
+      <span className="cover-letter-component__content">
+        Hello, I’m Gürkan. I started to my software developer career before
+        highschool. Until this time I developed many projects in different
+        platforms also i have chosen my path. I want to improve myself in java
+        backend development.
+      </span>
+    </div>
+  );
+};
+
+export default CoverLetterComponent;
