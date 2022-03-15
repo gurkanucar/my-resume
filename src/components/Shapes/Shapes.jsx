522250357e449f4bd9adfee3187e2561e7422f55
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-15 20:49:52 +0300

    animated shapes background implemented

--- src/components/Shapes/Shapes.jsx
+++ src/components/Shapes/Shapes.jsx
@@ -11,11 +11,17 @@ import Shape5 from "../../assets/shapes/Shape5.svg";
 const Shapes = () => {
   return (
     <div className="svg-shapes__root">
-      <img className="svg-shapes__item__1" src={Shape1} />
-      <img className="svg-shapes__item__2" src={Shape2} />
-      <img className="svg-shapes__item__3" src={Shape3} />
-      <img className="svg-shapes__item__4" src={Shape4} />
-      <img className="svg-shapes__item__5" src={Shape5} />
+      <img className="svg-shapes__item__1 svg-shapes__item__odd" src={Shape1} />
+      <img
+        className="svg-shapes__item__2 svg-shapes__item__even"
+        src={Shape2}
+      />
+      <img className="svg-shapes__item__3 svg-shapes__item__odd" src={Shape3} />
+      <img
+        className="svg-shapes__item__4 svg-shapes__item__even"
+        src={Shape4}
+      />
+      <img className="svg-shapes__item__5 svg-shapes__item__odd" src={Shape5} />
     </div>
   );
 };
