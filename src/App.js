104dc7ea6d195f8e6014952bc13308d526894f7a
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-15 22:41:10 +0300

    app js updated

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import LandingPageComponent from "./components/LandingPageComponent/LandingPageC
 import CoverLetterComponent from "./components/CoverLetterComponent/CoverLetterComponent";
 import ResumeTop from "./components/ResumeTop/ResumeTop";
 import ResumeComponent from "./components/ResumeComponent/ResumeComponent";
+import Navbar from "./components/Navbar/Navbar";
+import ContactButton from "./components/ContactButton/ContactButton";
+import DownloadButton from "./components/DownloadButton/DownloadButton";
 
 function App() {
   return (
     <div>
+      <Navbar>
+        <ContactButton />
+        <DownloadButton />
+      </Navbar>
       <LandingPageComponent />
       <ResumeComponent />
     </div>
