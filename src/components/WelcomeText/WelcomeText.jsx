319f1dfbb24e81361c8bbe2d10f35f7943222a2b
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-15 14:43:01 +0300

    welcome text component created

---
+++ src/components/WelcomeText/WelcomeText.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./WelcomeText.css";
+
+const WelcomeText = () => {
+  return (
+    <div className="welcome-text__root">
+      <div className="welcome-text__wrapper">
+        <span className="welcome-text__item">Hi there!</span>
+        <span className="welcome-text__item">
+          This is <strong className="welcome-text__item__strong">Gurkan</strong>
+        </span>
+        <span className="welcome-text__item">
+          I am a{" "}
+          <strong className="welcome-text__item__strong">Java Developer</strong>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeText;
