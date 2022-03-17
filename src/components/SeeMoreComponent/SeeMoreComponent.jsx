d64cca5b44c94ba82bf3062c008f4006a4179a7d
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-17 22:51:11 +0300

    ripple effect added to see more component

--- src/components/SeeMoreComponent/SeeMoreComponent.jsx
+++ src/components/SeeMoreComponent/SeeMoreComponent.jsx
@@ -7,7 +7,7 @@ const SeeMoreComponent = () => {
     <div className="see-more__root">
       <span className="see-more__text">see more about me...</span>
       <Scroll to="resume-view" offset={-70} smooth={true}>
-        <div className="see-more__icon">
+        <div className="see-more__icon ripple">
           <FaChevronDown size={40} />
         </div>
       </Scroll>
