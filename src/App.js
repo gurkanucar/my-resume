a3d30b69c3b5fce4b6bb26e4459e2ab0e94fc914
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-18 01:29:30 +0300

    LikeButtonComponent created and implemented

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import LandingPageComponent from "./components/LandingPageComponent/LandingPageC
 import ResumeComponent from "./components/ResumeComponent/ResumeComponent";
 import Navbar from "./components/Navbar/Navbar";
 import DownloadButton from "./components/DownloadButton/DownloadButton";
+import LikeButtonComponent from "./components/LikeButtonComponent/LikeButtonComponent";
 
 function App() {
   return (
     <div>
       <Navbar>
         {/* <ContactButton /> */}
+        <LikeButtonComponent />
         <DownloadButton />
       </Navbar>
       <LandingPageComponent />
