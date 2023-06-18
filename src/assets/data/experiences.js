b593f194f2eb7b417fd4bf41a45b5eb74d4bb133
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-18 13:56:47 +0300

    experiences updated

--- src/assets/data/experiences.js
+++ src/assets/data/experiences.js
@@ -1,15 +1,42 @@
 export const experiences = [
-    {
-    title: "Java Developer",
+  {
+    title: "Java Software Engineer",
+    company: "Yapikredi Bank via Metasis",
+    location: "Turkey",
+    jobDescription:
+      "Responsible for Monolith to Microservice conversion project using Java Spring Boot. Developing mock api tools using javascript express",
+    startDate: "05/2023",
+    endDate: "Present",
+  },
+  {
+    title: "Intern",
     company: "Huawei",
     location: "Turkey",
     jobDescription:
-      "Java, Spring Boot, MongoDB, PostgreSQL, Kafka, microservices",
+      "Developing microservice projects using spring boot, MongoDB, PostgreSQL, Kafka, Rancher.",
     startDate: "09/2022",
     endDate: "03/2023",
   },
   {
-    title: "Fullstack Developer",
+    title: "Intern",
+    company: "SugAR Tech",
+    location: "Turkey",
+    jobDescription:
+      "Java Spring Boot, Python Image processing, optimizing 2d to 3d image conversion.",
+    startDate: "08/2022",
+    endDate: "09/2022",
+  },
+  {
+    title: "Intern",
+    company: "IKnow Technology",
+    location: "Turkey",
+    jobDescription:
+      "I developed a microservice diary project. In this project, I used Spring Cloud Gateway, Feign Client, and Eureka Server.",
+    startDate: "06/2022",
+    endDate: "07/2022",
+  },
+  {
+    title: "Fullstack Developer Intern",
     company: "Bulut MD",
     location: "Turkey",
     jobDescription:
