eb07c6688c416981c43c209e529548c7fc0a161c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-03 09:20:34 +0300

    fix: resume download bug fixed

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -1,14 +1,14 @@
 import "./DownloadButton.css";
-
+import React from "react";
 import resume from "../../assets/resume.pdf";
 
 const DownloadButton = (props) => {
   return (
     <a href={resume} download="Gurkan_UCAR.pdf">
-      <div onClick={props.onClick} className="download-btn">
-        <span className="download-txt">Download CV</span>
-      </div>
-    </a>
+    <div onClick={props.onClick} className="download-btn">
+      <span className="download-txt">Download CV</span>
+    </div>
+  </a>
   );
 };
 
